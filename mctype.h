1ff1d2ae146fded2e6a7025aa469745ebfda6060
tobi <>
1999-01-04 22:32:42 +0000

    - added mctype.h because isdigit from ctype.h created bogus warnings with   emx 0.9 d ...

---
+++ mctype.h
@@ -0,0 +1,13 @@
+#ifndef __M_CTYPE_H
+#define __M_CTYPE_H
+
+#define m_isspace(x) ((x) == ' ' || (x) == '\t' || (x) == '\n' || (x) == '\r')
+#define m_isdigit(x) ((x) >= '0' && (x) <= '9')
+
+#endif
+
+
+
+
+
+
